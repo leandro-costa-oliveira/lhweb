6e1475552e9d34e8900178e72da809791697033a
loki <loki@lokibook>
2017-04-22 11:09:50 -0300

    - Adicionei um valor padrão para o texto do Label

--- inc/lhweb/view/LHFLabel.php
+++ inc/lhweb/view/LHFLabel.php
@@ -7,10 +7,13 @@ namespace lhweb\view;
  * @author loki
  */
 class LHFLabel extends LHFormField {
-    protected $text  = "LHFormLabel";
+    protected $text  = null;
     protected $width = 1;
     
     public function render() {
+        if($this->text === null) {
+            $this->text = ucwords($this->id) . ":";
+        }
         echo "<label for='$this->id' class='col-sm-$this->width control-label $this->class'";
         $this->renderData();
         echo ">";
